7addfe965c9f908a9920a981154c1c512828a356
tetsu-takumi <detsu.0113.soccer@icloud.com>
2025-08-03 23:31:19 +0900

    スクロール追加（要訂正）

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded',() => {
+    const scrollImg = new IntersectionObserver((inImages) => {
+        inImages.forEach((inImage) => {
+            if (inImage.isIntersecting) {
+                inImage.target.classList.add("display");
+            }else {
+                inImage.target.classList.remove("display");
+            }
+        });
+        }, {
+            threshold: 0.5,
+        });
+        document.querySelectorAll('.scroll').forEach((img) => {
+            scrollImg.observe(img);
+        });
+    });
