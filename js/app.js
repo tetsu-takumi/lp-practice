473381b80141cbad5fac57167a33b95f3af4489a
tetsu-takumi <detsu.0113.soccer@icloud.com>
2025-08-21 23:35:20 +0900

    スライダー、タイピング追加

--- js/app.js
+++ js/app.js
@@ -14,8 +14,30 @@ document.addEventListener('DOMContentLoaded',() => {
         document.querySelectorAll('.scroll').forEach((img) => {
             scrollImg.observe(img);
         });
-    });
+/*typing*/
+        const typingEl = document.getElementById("typing");
+    if (typingEl) {
+        const observer = new IntersectionObserver((entries, obs) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const text = typingEl.textContent;
+                    typingEl.textContent = "";
+                    let i = 0;
+                    function typingEffect() {
+                        if (i < text.length) {
+                            typingEl.textContent += text.charAt(i);
+                            i++;
+                            setTimeout(typingEffect, 300); 
+                        }
+                    }
+                    typingEffect();
+                }
+            });
+        }, { threshold: 0.5 });
+        observer.observe(typingEl);
+    }
+});
 
-    /*swiper*/
 
-    
+
+
